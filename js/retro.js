ac081aceb3a332a5d46ec0ba93e5915be133e558
Mimu <mimu11@gmail.com>
2024-11-08 20:10:53 -1200

    final project

--- js/retro.js
+++ js/retro.js
@@ -16,13 +16,12 @@ tooltipIcons.forEach((icon, index) => {
   })
 })
 // load data from api
-const loadData = async (searchText = "comedy") => {
+const loadData = async (searchText = 'comedy') => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`
   );
   const data = await res.json();
   const posts = data.posts;
-  // console.log(posts)
   displayPosts(posts);
 };
 // display post
@@ -33,62 +32,68 @@ const displayPosts = (posts) => {
     const postCard = document.createElement("div");
     postCard.classList = ` p-4`;
     postCard.innerHTML = `
-        <div class="lg:flex gap-10 rounded-xl p-10 bg-sky-100 shadow-xl">
+        <div class="space-y-6 lg:space-y-0 lg:flex justify-between  gap-10 rounded-xl p-10 bg-sky-100 shadow-xl">
              <div class="lg:w-[72px] lg:h-[72px]: relative">
                 <img src="${post.image}"alt="Movie" />
-                <div id="active-posts" class="bg-green-500 absolute ml-16 -mt-20 rounded-full w-4 h-4"></div>
+                <div id="active-posts" class="bg-green-500 absolute -ml-1 lg:ml-16 -mt-64 lg:-mt-20 rounded-full w-6 lg:w-4 h-6 lg:h-4"></div>
             
             </div>
-             <div class="space-y-6 ">
+             <div class="space-y-6">
                 <p># ${post.category} <span class="ml-4">Author:</span> ${post.author.name}</p>
                 <h2 class="card-title">${post.title}</h2>
                 <p>${post.description}</p>
                 <p class="space-x-5"><i class="fa-regular fa-message"></i><span>${post.comment_count}</span><i class="fa-regular fa-eye"></i>
                         <span>${post.view_count}</span><i class="fa-solid fa-clock"></i><span>${post.posted_time}</span></p>  
                      </div>
-            <div class="flex items-end justify-end">
-                      <button onclick="handleShowTitle(${posts.title})" class="btn btn-accent">Title</button>
+            <div class="flex items-end">
+                  <i onclick="handleShowTitle(${posts.title})" class="fa-regular fa-envelope-open text-3xl text-green-500"></i>
             </div>
         </div>
         `;
     postContainer.appendChild(postCard);
     const activePost = document.getElementById('active-posts')
-    console.log(activePost)
-
-
-    if (post.isActive === false) {
-      activePost.classList.add('bg-green-500')
+    // console.log(activePost)
+    // console.log(post)
+    const getActive = post.isActive
+    if (getActive) {
+      activePost.classList.add('bg-green-500');
     }
-
-
+    else {
+      activePost.classList.add('bg-orange-500');
+    }
   });
   toggleLoadingSpinner(false);
 };
-const handleShowTitle = async (posts) => {
-  console.log(posts)
+const handleShowTitle = async () => {
+  console.log()
   const res = await fetch(
     `https://openapi.programming-hero.com/api/retro-forum/posts`
   );
   const data = await res.json();
   const post = data.posts[0];
-  // console.log(post);
+  console.log(post);
   showPostTitle(post);
 };
+
+
 // show post title
 const showPostTitle = (post) => {
-  const showTitleContainer = document.getElementById("show-title-container");
   console.log(post)
-  showTitleContainer.classList.remove("hidden");
-  showTitleContainer.innerHTML = `
-           <h2 class="card-title">${post.title}</h2>
-           <p><i class="fa-regular fa-eye"></i><span>${post.view_count}</span></p>
-      `;
+  const showTitleContainer = document.getElementById("show-title-container");
+  showTitleContainer.classList.remove('hidden')
+  const titleContainer = document.createElement('div');
+  titleContainer.innerHTML = `
+  <div class="flex justify-between bg-slate-300 rounded-xl mt-4 p-6">
+  <h2 class="card-title">${post.title}</h2>
+  <p><i class="fa-regular fa-eye"></i><span>${post.view_count}</span></p>
+  </div>
+    `;
+  showTitleContainer.appendChild(titleContainer);
   const readCount = document.getElementById("read-count");
   const readCountText = readCount.innerText;
   const readCountValue = parseInt(readCountText);
   const latestRead = readCountValue + 1;
   readCount.innerText = latestRead;
-
 };
 // search handle
 const searchHandle = () => {
@@ -117,12 +122,11 @@ const loadCard = async () => {
   const cards = data;
   displayCard(cards);
 };
-
+// display card
 const displayCard = (cards) => {
   const cardContainer = document.getElementById("card-container");
   cards.forEach((card) => {
     const creatCard = document.createElement("div");
-
     creatCard.innerHTML = `
         <div class="card lg:w-96 lg:h-[500px] p-6 space-y-8 bg-base-200 shadow-xl">
   <figure class="px-10 pt-10">
@@ -131,7 +135,6 @@ const displayCard = (cards) => {
       alt="Shoes"
       class="rounded-xl" />
   </figure>
-  
   <p> <i class="fa-solid fa-calendar-week"></i><span id="date"> ${card.author.posted_date}</span></p>
 <h4 class="text-xl font-bold">${card.title}</h4>
 <p>${card.description}</p>
@@ -152,3 +155,4 @@ const displayCard = (cards) => {
 
 loadData();
 loadCard();
+
